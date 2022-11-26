7e1a0169f5fdb2fb81e6105ce7fcb374b572c0a0
Wahyu Ramadan <wramadan1203@gmail.com>
2022-11-26 10:14:40 +0700

    split every space

---
+++ commands/log.js
@@ -0,0 +1,30 @@
+import fs from 'fs';
+
+const defaultFile = 'C:/Windows/Panther/setuperr.log';
+
+const readFile = () => {
+  // Read file and convert to string then slice per \n
+  const file = fs.readFileSync(defaultFile).toString().trim().split('\n');
+
+  let arrayDefault = [];
+
+  file.forEach((valueOfFile, indexFile, arrayFile) => {
+    let data = {
+      dateTime: '',
+      logLevel: '',
+      loggingComponent: '',
+      message: '',
+    };
+    let loggingComponent = '';
+    let message = '';
+
+    // Create content to array and get every index of arrayFile then split when have space
+    const splitingSpace = arrayFile[indexFile].split(/\s+/);
+
+    console.log('splitingSpace', splitingSpace);
+  });
+};
+
+export default {
+  readFile,
+};
