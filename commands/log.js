e1c0b046216eb82ac054a7efe491aa131da0b260
Wahyu Ramadan <wramadan1203@gmail.com>
2022-11-26 11:11:27 +0700

    use condition to get message and logging component

--- commands/log.js
+++ commands/log.js
@@ -9,7 +9,7 @@ const readFile = () => {
   let arrayDefault = [];
 
   file.forEach((valueOfFile, indexFile, arrayFile) => {
-    let data = {
+    const data = {
       dateTime: '',
       loggingLevel: '',
       loggingComponent: '',
@@ -20,14 +20,26 @@ const readFile = () => {
 
     // Create content to array and get every index of arrayFile then split when have space
     const splitingSpace = arrayFile[indexFile].split(/\s+/);
-    console.log(splitingSpace);
 
-    // Set message
-    // Choose index start use splice then join with the next element
-    message = splitingSpace.splice(3).join(' ');
+    // Check array index 3 is start with [ or not
+    if (splitingSpace[3].startsWith('[')) {
+      // Set logging component from array splitingSpace index 4
+      loggingComponent = splitingSpace[4];
+      // Set message
+      // Choose index start use splice then join with the next element
+      message = splitingSpace.splice(5).join(' ');
+    } else {
+      // Set logging component from array splitingSpace index 3
+      loggingComponent = splitingSpace[3];
+      // Set message
+      // Choose index start use splice then join with the next element
+      message = splitingSpace.splice(4).join(' ');
+    }
+
     // Set value from spliting to on key
     data.dateTime = `${splitingSpace[0]} ${splitingSpace[1]}`;
     data.loggingLevel = splitingSpace[2];
+    data.loggingComponent = loggingComponent;
     data.message = message;
 
     console.log('data', data);
